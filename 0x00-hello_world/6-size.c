2d87c51c09bf091ddc9d0766d5f520ce7346b3cc
Matias <lxpirez133@gmail.com>
2020-09-16 17:05:45 +0000

    not type just the variable

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,17 +5,10 @@
  */
 int main(void)
 {
-char charType;
-int intType;
-long longType;
-longlongint longlongintType;
-float floatType;
-
-/*sizeof evaluates the size of a variable*/
-printf("Size of char: %ld byte(s)\n", sizeof(charType));
-printf("Size of int: %ld byte(s)\n", sizeof(intType));
-printf("Size of long int: %ld byte(s)\n", sizeof(longintType));
-printf("Size of long long int: %ld byte(s)\n", sizeof(longlongintType));
-printf("Size of float: %ld byte(s)\n", sizeof(floatType));
+printf("Size of char: %ld byte(s)\n", sizeof(char));
+printf("Size of int: %ld byte(s)\n", sizeof(int));
+printf("Size of long int: %ld byte(s)\n", sizeof(long int));
+printf("Size of long long int: %ld byte(s)\n", sizeof(long long int));
+printf("Size of float: %ld byte(s)\n", sizeof(float));
 return (0);
 }
