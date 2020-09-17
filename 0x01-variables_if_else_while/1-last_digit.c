b793fa1d0cee69c3ab7a04b2809924c182d1ec9c
Matias <lxpirez133@gmail.com>
2020-09-17 21:53:40 +0000

    wrong wording exercise 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,7 +27,7 @@ printf("Last digit of %d is %d and is 0\n", n, LastDigit);
 }
 else if (LastDigit < 6)
 {
-printf("Last digit of %d is %d and is 0\n", n, LastDigit);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, LastDigit);
 }
 return (0);
 }
