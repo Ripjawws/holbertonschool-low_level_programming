fc4aee4297c429627fe5ca9db9bc20c76ac56d92
Matias <lxpirez133@gmail.com>
2020-09-17 18:01:19 +0000

    wrong name

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch
+char b
+for (ch = 'a'; ch <= 'z'; ch++)
+for (b = 'A'; b <= 'Z'; ch++)
+putchar(ch);
+putchar(b);
+putchar('\n');
+return (0);
+}
