06e097890e24f3eb1716e9f0a6437f55e05847c8
Matias <lxpirez133@gmail.com>
2020-09-17 20:04:51 +0000

    exercise 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+char b;
+for (c = '0'; c <= '9'; c++)
+putchar (c);
+for (b = 'a'; b <= 'f'; b++)
+putchar (b)
+putchar ('\n');
+return (0);
+}
