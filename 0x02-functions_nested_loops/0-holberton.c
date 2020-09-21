a7653e5cb89d80b54d9e6815ab31953ba8d24411
Matias <lxpirez133@gmail.com>
2020-09-21 14:12:46 +0000

    first push of the day

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ * Description: Use function _putchar to print Holberton on the screen
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char holberton[] = "Holberton";
+int i = 0;
+while (holberton[i] != '\0')
+{
+_putchar(holberton[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+functions,header,function prototype
