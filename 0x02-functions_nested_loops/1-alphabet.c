9831bf19bb92e40fef6ab52a48481e234fe74421
Matias <lxpirez133@gmail.com>
2020-09-21 15:55:36 +0000

    trying to push

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,14 @@
+#include <stdio.h>
 #include "holberton.h"
-
 /**
  * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
-print_alphabet();
-return (0);
+char ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar ('\n');
 }
