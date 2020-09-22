1a233f44c2a122aea99e63ab5eb6b4f9ebc2a8a6
Matias <lxpirez133@gmail.com>
2020-09-22 21:15:31 +0000

    hotfix

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,14 +7,15 @@
  */
 
 long int main(void)
-{ 
-long int f1 = 1, f2 = 2, i;  
-for (i = 1; i <= 50; i++) 
-{ 
-printf("%ld, ", f1); 
-long int next = f1 + f2; 
-f1 = f2; 
+{
+long int f1 = 1, f2 = 2, i;
+for (i = 1; i <= 50; i++)
+if ("%ld", i <= 50)
+{
+printf("%ld, ", f1);
+long int next = f1 + f2;
+f1 = f2;
 f2 = next;
-}  
-return 0; 
-} 
+}
+return (0);
+}
