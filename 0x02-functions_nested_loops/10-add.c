2f5ee57e1e7ea6684ec53f4a78a1d6c4b8302bf6
Matias <lxpirez133@gmail.com>
2020-09-22 16:21:37 +0000

    exercise 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * add - take a sum of 2 numbers
+ * @n1: first number
+ * @n2: second number
+ * Return: nothing
+ */
+int add(int n1, int n2)
+{
+return (n1 + n2);
+}
