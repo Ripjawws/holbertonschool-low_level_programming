82fe6f22d0897719654be8cae4eb4c7be5601c3f
Matias <lxpirez133@gmail.com>
2020-09-24 18:43:20 +0000

    hotfix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,30 +2,26 @@
 #include "holberton.h"
 
 /**
- * print_line - Entry point
- * Description: print n of '_' on screen, followed by new line.
- * 0 and negative number will be print new line.
- * @n: number of '_' is requested by main.c
- * Return: nothing
+ * print_diagonal - draws diagonal line
+ * @n: number of is requested by main.c
  */
 void print_diagonal(int n)
 {
-  int i;
-  int m = 0;
-
-  if (n > 0)
-    {
-      while (m != n)
-	{
-	  for (i = 0; i < m; i++)
-	    {
-	      _putchar(' ');
-	    }
-	  _putchar('\\');
-	  _putchar('\n');
-	  m++;
-	}
-    }
-  else
-    _putchar('\n');
+int i;
+int m = 0;
+if (n > 0)
+{
+while (m != n)
+{
+for (i = 0; i < m; i++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+m++;
+}
+}
+else
+_putchar('\n');
 }
