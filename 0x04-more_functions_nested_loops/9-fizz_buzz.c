d2d5bc6f910ee9e6fc696c74efa03c9b4e0af35a
Matias <lxpirez133@gmail.com>
2020-09-24 19:20:35 +0000

    hotfix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,35 +1,29 @@
 #include <stdio.h>
+#include "holberton.h"
+
 /**
- *main - entry
+ * main - Entry point
+ * Description: check FizzBuzz num from 1 to 100;.
+ * Fizz for num completly divided by 3, Buzz for 5 and
+ * FizzBuzz for both.
  *
- *Return: Always 0.
+ * Return: 0 Always
  */
 int main(void)
 {
 int i;
 for (i = 1; i <= 100; i++)
 {
-if ((i % 3) == 0 && (i % 5) == 0)
-{
+if (i % 15 == 0)
 printf("FizzBuzz");
-}
-else if ((i % 3) == 0)
-{
-printf("Fizz ");
-}
-else if ((i % 5) == 0)
-{
-printf("Buzz ");
-}
-else if (i == 100)
-{
-printf("Buzz ");
-printf("\n");
-}
+else if (i % 3 == 0)
+printf("Fizz");
+else if (i % 5 == 0)
+printf("Buzz");
 else
-{
-printf("%d ", i);
-}
+printf("%i", i);
+if (i < 100)
+printf(" ");
 }
 printf("\n");
 return (0);
