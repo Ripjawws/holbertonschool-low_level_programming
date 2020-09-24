d67f87adbfaa7c014152cab6415a3398dac8d342
Matias <lxpirez133@gmail.com>
2020-09-24 18:51:02 +0000

    exercise

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_square - Entry point
+ * Description: print square of # size n. followed by new line
+ * 0 and negative number will be print new line.
+ * @size: number of size that is requested by main.c
+ *
+ * Return: nothing
+ */
+void print_square(int size)
+{
+int i, j;
+if (size <= 0)
+_putchar('\n');
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
