fdf989a5fdc56c2253187f69da394901371eb255
Matias <lxpirez133@gmail.com>
2020-10-01 19:22:43 +0000

    my friend betty

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,19 +11,17 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-  int l;
-  int i;
-  for ( l = 0; dest[l] != '\0'; l++)
-    {
-    }
-  for (i = 0; src[i] != '\0'; i++)
-    {
-    }
-  for ( i = 0; i < n && src[i] != '\0'; i++)
-    {
-      dest [l + i] = src[i];
-
-    }
-  return (dest);
-
+int l;
+int i;
+for (l = 0; dest[l] != '\0'; l++)
+{
+}
+for (i = 0; src[i] != '\0'; i++)
+{
+}
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest [l + i] = src[i];
+}
+return (dest);
 }
--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,9 +13,12 @@ void reverse_array(int *a, int n)
 	int temp = 0;
 	int i;
 	int j;
-	for ( i = 0; i < n; i++)
+
+	for (i = 0; i < n; i++)
+
 	{
 	}
+
 	j = i - 1;
 	i = 0;
 	while (i < j)
