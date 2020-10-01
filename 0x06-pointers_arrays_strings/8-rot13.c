308e9f3679494574ee4f20365bc3f97ef24ec486
Matias <lxpirez133@gmail.com>
2020-10-01 19:09:29 +0000

    exercise 8

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * rot13 - function that encodes a string using rot13
+ * @str: character
+ * Return: string in rot13
+ */
+char *rot13(char *str)
+{
+	int i;
+	int j;
+	char from[] = "abcdefghijklmABCDEFGHIJKLM";
+	char to[] = "nopqrstuvwxyzNOPQRSTUVWXYZ";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; from[j] != '\0'; j++)
+		{
+			if (str[i] == from[j])
+			{
+				str[i] = to[j];
+			}
+			else if (str[i] == to[j])
+			{
+				str[i] = from[j];
+			}
+		}
+	}
+	return (str);
+}
