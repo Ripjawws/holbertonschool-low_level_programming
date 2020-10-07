9166752af5136d4422936be01a990826e06b07c3
Matias <lxpirez133@gmail.com>
2020-10-07 15:48:05 +0000

    first commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - Entry point
+ * Description: use recursion to return length of string
+ * @s: string that need to be printed
+ *
+ * Return: length of s
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		return(0);
+	}
+	return (_strlen_recursion(s+1)+1);
+}
