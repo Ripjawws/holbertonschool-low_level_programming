e0c040a117e3c0251d26e34736619518cf7fbbbe
Matias <lxpirez133@gmail.com>
2020-10-08 12:38:23 +0000

    hotfix

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -5,6 +5,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int natural(int i, int j);
 int is_prime_number(int n);
 int helper_prime(int i, int n);
 int is_palindrome(char *s);
