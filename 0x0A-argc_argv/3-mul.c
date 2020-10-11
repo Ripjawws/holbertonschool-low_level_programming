3260fe72635c93bd8afe17e7e2d90555942f0da2
Ripjawws <lxpirez133@gmail.com>
2020-10-11 22:45:26 +0000

    exercise3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - main
+ *@argc: argc
+ *@argv: argv
+ *Return: 0
+ **/
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+int num1, num2, result;
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+result = num1  *num2;
+printf("%d\n", result);
+return (0);
+}
