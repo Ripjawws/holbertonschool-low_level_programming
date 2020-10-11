18f6015993c3ab80cbbb3da12c7b5066e8564ce1
Ripjawws <lxpirez133@gmail.com>
2020-10-11 22:43:28 +0000

    number 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *main - main
+ *@argc: argc
+ *@argv: argv
+ *Return: 0
+ **/
+int main(int argc, char *argv[])
+{
+int count;
+for (count = 0; count < argc; count++)
+{
+printf("%s\n", argv[count]);
+}
+return (0);
+}
