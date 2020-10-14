23295371764588c8e3bd779c730da61be6583125
Matias <lxpirez133@gmail.com>
2020-10-14 17:07:13 +0000

    exercise 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,14 +10,32 @@
 
 char *_strdup(char *str)
 {
+	int len1 = 0;
+	int len2 = 0;
 	char *p;
 
-p = malloc (strlen (str) + 1);
-	if (p != NULL)
+	if (str == 0)
 	{
-	strcpy (p,str);
-	return (p);
+		return (NULL);
+	}
+	while (str[len1] != 0)
+	{
+		len1++;
 	}
 
+	p = (char *)malloc(len1 + 1 * sizeof(char));
+
+	if (p == 0)
+	{
+
 	return (NULL);
+
+	}
+
+	for (len2 = 0; len2 < len1; len2++)
+
+	{
+		p[len2] = str[len2];
+         }
+	return (p);
 }
