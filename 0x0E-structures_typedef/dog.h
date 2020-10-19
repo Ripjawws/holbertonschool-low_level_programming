971df490110200b3948ac4ab726d78f6493dc710
Matias <lxpirez133@gmail.com>
2020-10-19 17:04:50 +0000

    hotfix 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,5 +13,6 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 int _putchar(char c);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /* DOG_H */
