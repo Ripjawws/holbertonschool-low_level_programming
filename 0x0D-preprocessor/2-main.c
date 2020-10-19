db30e65edf758065712cdca8e0ee3f1e971b6a08
Matias <lxpirez133@gmail.com>
2020-10-19 15:42:42 +0000

    hotfix

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - file
- * return 0
- **/
-
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 int main(void)
-
 {
-
-printf("%s\n",__BASE_FILE__);
-
-return (0);
-
+  printf("%s\n", __BASE_FILE__);
+  return (0);
 }
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef THIRDHEADER_H
 #define THIRDHEADER_H
 
-#define ABS(x) ((x)<0 ? (-x) : (x))
+#define ABS(x) ((x)<0 ? -(x) : (x))
 
 #endif /* THIRDHEADER_H */
