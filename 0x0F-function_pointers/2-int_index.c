b77a33917d19ca113a51394e89383488718c52fa
Matias <lxpirez133@gmail.com>
2020-10-21 19:08:08 +0000

    exercise

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,10 +2,38 @@
 #include "function_pointers.h"
 
 /**
- * is_98 - check if a number is equal to 98
- * @elem: the integer to check
+ * int_index - Entry point
+ * Description: searching matched integer in array.
+ * @array: array of integer
+ * @size: size of array
+ * @cmp: function pointer to another function.
  *
- * Return: 0 if false, something else otherwise.
+ * Return: return index of the first element which matches
  */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int i;
+
+	if (array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		cmp(array[i]);
+
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+
+	}
+	return (-1);
+
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - Entry point
+ * Description: print the result
+ * @argc: number of argument
+ * @argv: array of strings
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int n1, n2;
+	char c = argv[2][0];
+	char c1 = argv[2][1];
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (c1 != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (c != '+' && c != '-' && c != '*' && c != '/' && c != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
+	printf("%i\n", (*get_op_func(argv[2]))(n1, n2));
+	return (0);
+}
