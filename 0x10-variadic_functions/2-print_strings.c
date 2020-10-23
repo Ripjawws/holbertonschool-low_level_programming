7b5cc560e53c51fd6092fe9acf900b1b42a19fa9
Matias <lxpirez133@gmail.com>
2020-10-23 15:46:27 +0000

    exercise 3

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *can;
+
+	va_list list;
+
+	va_start (list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		can = va_arg(list, char*);
+		if (can == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", can);
+		}
+	if (separator != NULL && i < (n - 1))
+	{
+		printf("%s", separator);
+	}
+	}
+	va_end(list);
+	printf("\n");
+}
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - Entry point
+ * Description: print all type of arguments
+ * @format: abbreviation of type
+ * @...: elipses
+ *
+ * Return: nothings
+ */
+void print_all(const char * const format, ...)
+{
+	unsigned int j = 0;
+	int chk = 0;
+	va_list arguments;
+	char c;
+	char *s;
+
+	while (format == NULL)
+	{
+		return;
+	}
+	va_start(arguments, format);
+	while (format[j] != '\0' && format)
+	{
+		c = format[j];
+		if (chk != 0 && (c == 'c' || c == 'i' || c == 'f' || c == 's'))
+			printf(", ");
+		switch (c)
+		{
+		case 'i':
+			printf("%i", va_arg(arguments, int));
+			j++;
+			chk = 1;
+			break;
+		case 'f':
+			printf("%f", va_arg(arguments, double));
+			j++;
+			chk = 1;
+			break;
+		case 'c':
+			printf("%c", va_arg(arguments, int));
+			j++;
+			chk = 1;
+			break;
+		case 's':
+			s = va_arg(arguments, char *);
+			if (s == NULL)
+				s = "(nil)";
+			printf("%s", s);
+			j++;
+			chk = 1;
+			break;
+		default:
+			j++;
+			break;
+		}
+	}
+	printf("\n");
+	va_end(arguments);
+}
