74d187571edc081bc0f3ce5c81a1b1bd48e96caa
Matias <lxpirez133@gmail.com>
2020-10-24 23:56:23 +0000

    advance 100asm

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,25 +1,13 @@
-%define sys_write 1
-	%define stdout 1
+section .data
 
-	%define sys_exit 60
-	%define success 0
-
-	%define nl 10
-
-	section .data
-
-	    message db "Hello, world!", nl
+	    message db "Hello, Holberton", 10
 
 	section .text
 
-	global _start
-_start:
-	    mov rax, sys_write
-	    mov rdi, stdout
+	global _main
+_main:
+	    mov rax, 1
+	    mov rdi, 1
 	    mov rsi, message
 	    mov rdx, 17
-	    syscall
-
-	    mov rax, sys_exit
-	    mov rdi, success
 	    syscall
