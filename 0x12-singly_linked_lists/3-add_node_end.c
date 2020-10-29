0e108da0249818e36658399a71b165182ddfc037
Matias <lxpirez133@gmail.com>
2020-10-29 17:25:34 +0000

    last 2

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_node_end - Entry point
+ * Description: print number of element in the linked list
+ * @head: first node
+ * @str: content of string
+ * Return: node.
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *p;
+	int i = 0;
+
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	new->str = strdup(str);
+	new->len = i;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	for (p = *head; p->next != NULL; p = p->next)
+		;
+	p->next = new;
+	return (new);
+}
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - Entry point
+ * Description: free list_t
+ * @head: first node
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	while (head != NULL)
+	{
+		free(head->str);
+		free(head);
+		head = head->next;
+	}
+}
