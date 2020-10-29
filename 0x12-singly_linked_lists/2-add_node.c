85a6f62397fd4b676f887a543417e3a54dedeacd
Matias <lxpirez133@gmail.com>
2020-10-29 16:37:43 +0000

    exercise 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_node - Entry point
+ * Description: print number of element in the linked list
+ * @head: first node
+ * @str: content of string
+ * Return: node.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	int runner;
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+        while (str[runner] != '\0')
+	{
+		runner++;
+	}
+	new->len = runner;
+	new->str = strdup(str);
+	new->next = *head;
+	*head = new;
+
+	return (*head);
+}
