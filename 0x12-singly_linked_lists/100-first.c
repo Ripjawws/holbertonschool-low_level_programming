c5d0106fbd032dd3a1056085fb3f31e4f5d0f66a
Matias <lxpirez133@gmail.com>
2020-10-29 17:53:19 +0000

    fixed betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
 /**
- * Apply the constructor attribute to before_main() so that it
-   is executed before main()
- *before_main - execute before
+ * before - executes
+ *
+ *
  */
-void __attribute__ ((constructor)) before_main()
+void __attribute__ ((constructor)) before()
 {
 	printf("You're beat! and yet, you must allow\n");
 	printf("I bore my house upon my back!\n");
