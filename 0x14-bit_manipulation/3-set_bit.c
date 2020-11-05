2f6b382aaa56400f115851b6f58c8aa79628ad7e
Matias <lxpirez133@gmail.com>
2020-11-05 16:39:38 +0000

    betty

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include "holberton.h"
 /**
+ * set_bit - Entry point
+ * Description: set 1 to bit at given index
+ * @n: base 2 number
+ * @index: index of bit will be checked
+ * Return: nothing.
  */
+
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index < sizeof(n) * 8)
