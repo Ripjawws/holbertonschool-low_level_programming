7dd3cebf7eb241ea28187ab223cf496e653283f8
Matias <lxpirez133@gmail.com>
2020-11-09 23:36:07 +0000

    needed headers

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,5 +1,12 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
