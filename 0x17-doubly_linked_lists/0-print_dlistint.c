7b64964cd506eb2d0e49540c23b0ebeab71a783c
Matt <lxpirez133@gmail.com>
2020-12-11 13:49:44 -0300

    first exercise

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - Print
+ *@h: Variable
+ * Return: Always EXIT_SUCCESS.
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+size_t i = 0;
+const dlistint_t *temp;
+temp = h;
+
+while (temp)
+{
+printf("%d\n", temp->n);
+temp = temp->next;
+i++;
+}
+return (i);
+}
