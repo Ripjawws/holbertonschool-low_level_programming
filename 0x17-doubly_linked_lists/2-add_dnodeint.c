cdfe7c076a2f12ecdf29db58b6165c3047ec598c
Matias <lxpirez133@gmail.com>
2020-12-14 16:12:05 +0000

    re-uploading

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint - check the code for Holberton School students.
+ *@head: head
+ *@n: n
+ * Return: Always EXIT_SUCCESS.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *node = malloc(sizeof(dlistint_t));
+
+	if (node == NULL)
+		return (NULL);
+	node->n = n;
+	node->next = (*head);
+	node->prev = NULL;
+
+	if ((*head) != NULL)
+		(*head)->prev = node;
+
+	(*head) = node;
+	return (*head);
+}
