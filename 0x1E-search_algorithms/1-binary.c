dfcb6e7bdf7542751e4e1b5c2954022fafd1b48f
Matias <lxpirez133@gmail.com>
2021-04-22 13:33:12 +0000

    exercises

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,37 @@
+#include "search_algos.h"
+/**
+ *binary_search - linear search
+ *@array: array
+ *@size: size
+ *@value: value
+ *Return: number of nodes
+ */
+int binary_search(int *array, size_t size, int value)
+{
+    unsigned int lowerLimit = 0;
+    unsigned int upperLimit = size - 1;
+    unsigned int middle, i;
+
+    if (array == NULL)
+        return (-1);
+    while (lowerLimit <= upperLimit)
+    {
+        printf("Searching in array: ");
+        for (i = lowerLimit; i <= upperLimit; i++)
+        {
+            printf("%d", array[i]);
+            if (i < upperLimit)
+                printf(", ");
+            else
+                printf("\n");
+        }
+        middle = (lowerLimit + upperLimit) / 2;
+        if (array[middle] == value)
+            return (middle);
+        if (array[middle] < value)
+            lowerLimit = middle + 1;
+        if (array[middle] > value)
+            upperLimit = middle - 1;
+    }
+    return (-1);
+}
